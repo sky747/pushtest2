bc27d4a1b8259be2fe55de4a84b56eb0a410e5cf
Taiki <t9034869@icloud.com>
2022-07-14 16:47:34 +0900

    User name added

--- Game.java
+++ Game.java
@@ -4,8 +4,12 @@ public class Game {
     int heads = 0;
     int tails = 0;
 
-    System.out.println("Tossing a coin...");
+    System.out.println("Who are you?");
+    System.out.print(">");
     Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+    System.out.println("Hello," + name + "!");
+    System.out.println("Tossing a coin...");
     for(int i = 1 ; i <= 3 ; i++){
       System.out.print("Round" + i +":");
       String round = sc.next();
